2b3fc4ad28a573c602c1479725d4ed0ade8c1af8
Mary <schmitt.mary7@gmail.com>
2018-01-21 19:48:54 -0700

    added json package as well as half of the mutators and accessors

--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -23,7 +23,7 @@ use Ramsey\Uuid\Uuid;
  *
  **/
 
-class Profile {
+class Profile implements \JsonSerializable {
     use ValidateUuid;
     /**
      * ID for this profile. This will be the primary key.
@@ -65,3 +65,88 @@ class Profile {
 public function getProfileId() : Uuid {
     return($this->profileId);
 }
+
+/**
+ * mutator method for profile ID
+ *
+ * @param Uuid/string $newProfileId new value of profile ID
+ * @throws \RangeException if $newProfileId is not positive
+ * @throws \TypeError if $newProfileId is not a uuid or string
+ **/
+public function setProfileId($newProfileId) : void {
+    try {
+        $uuid = self::validateUuid($newProfileId);
+    } catch (\InvalidArgumentException | \ RangeException | \Exception | \TypeError $exception) {
+        $exceptionType = get_class($exception);
+        throw(new $exceptionType($exception->getMessage(), 0, $exception));
+    }
+/**
+ * convert and store the profile ID
+ **/
+    $this->profileId = $uuid;
+}
+
+/**
+ *accessor method for profile email
+ * @return string value of profile email
+ **/
+
+public function getProfileEmail() : string{
+    return($this->profileEmail);
+}
+
+/**
+ * mutator method for profile email
+ *
+ * @param string $newProfileEmail new value of profile email
+ * @throws \RangeException if $newProfileEmail is > 120 characters
+ * @throws \TypeError if $newProfileEmail is not a string
+ * @throws \InvalidArgumentException if $newProfileEmail is invalid or insecure
+ **/
+
+public function setProfileEmail($newProfileEmail) : void {
+    $newProfileEmail = trim($newProfileEmail);
+    $newProfileEmail = filter_var($newProfileEmail, FILTER_VALIDATE_EMAIL);
+    if(empty($newProfileEmail) === true) {
+        throw(new \InvalidArgumentException("profile email is empty or insecure"));
+    }
+    if(strlen($newProfileEmail) > 128) {
+        throw(new \RangeException("profile email is too large"));
+    }
+    $this->profileEmail = $newProfileEmail;
+}
+
+/**
+ * accessor method for profileHash
+ * @return value of profile hash
+ **/
+
+public function getProfileHash() {
+    return($this->profileHash);
+}
+
+/**
+ * mutator method for profile hash
+ *
+ * @param $newProfileHash
+ * @throws \InvalidArgumentException if the hash is not secure
+ * @throws \RangeException if the hash is not 128 characters
+ * @throws \TypeError if profile hash is not a string
+ **/
+public function setProfileHash(string $newProfileHash) : void {
+    $newProfileHash = trim($newProfileHashds);
+    $newProfileHash = strtolower($newProfileHash);
+    if(empty($newProfileHash) === ) {
+        throw(new \InvalidArgumentException("profile hash empty or insecure"));
+    }
+    if(!ctype_xdigit($newProfileHash)) {
+        throw(new \RangeException("profile hash must be 128 characters"));
+    }
+    $this->profileHash = $newProfileHash;
+}
+
+/**
+ * accessor method for profile name
+ *
+ */
+
