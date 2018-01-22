559f3cda619aa396fa2a4546bc1d76ed5802686f
Mary <schmitt.mary7@gmail.com>
2018-01-21 20:45:05 -0700

    found the rogue brace and moved it.

--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -23,7 +23,8 @@ use Ramsey\Uuid\Uuid;
  *
  **/
 
-class Profile implements \JsonSerializable {
+class Profile implements \JsonSerializable
+{
     use ValidateUuid;
     /**
      * ID for this profile. This will be the primary key.
@@ -55,176 +56,189 @@ class Profile implements \JsonSerializable {
      * @var string $profileStatement
      **/
     private $profileStatement;
-}
 
-/**
- * accessor method for profile ID
- * @return Uuid value of profile ID
- **/
+    /**
+     * accessor method for profile ID
+     * @return Uuid value of profile ID
+     **/
 
-public function getProfileId() : Uuid {
-    return $this->profileId;
-}
-
-/**
- * mutator method for profile ID
- *
- * @param Uuid| string $newProfileId new value of profile ID
- * @throws \RangeException if $newProfileId is not positive
- * @throws \TypeError if $newProfileId is not a uuid or string
- **/
-public function setProfileId($newProfileId) : void {
-    try {
-        $uuid = self::validateUuid($newProfileId);
-    } catch (\InvalidArgumentException | \ RangeException | \Exception | \TypeError $exception) {
-        $exceptionType = get_class($exception);
-        throw(new $exceptionType($exception->getMessage(), 0, $exception));
-    }
-/**
- * convert and store the profile ID
- **/
-    $this->profileId = $uuid;
-}
-
-/**
- *accessor method for profile email
- * @return string value of profile email
- **/
-
-public function getProfileEmail() : string{
-    return($this->profileEmail);
-}
-
-/**
- * mutator method for profile email
- *
- * @param string $newProfileEmail new value of profile email
- * @throws \RangeException if $newProfileEmail is > 120 characters
- * @throws \TypeError if $newProfileEmail is not a string
- * @throws \InvalidArgumentException if $newProfileEmail is invalid or insecure
- **/
-
-public function setProfileEmail($newProfileEmail) : void {
-    $newProfileEmail = trim($newProfileEmail);
-    $newProfileEmail = filter_var($newProfileEmail, FILTER_VALIDATE_EMAIL);
-    if(empty($newProfileEmail) === true) {
-        throw(new \InvalidArgumentException("profile email is empty or insecure"));
-    }
-    if(strlen($newProfileEmail) > 128) {
-        throw(new \RangeException("profile email is too large"));
-    }
-    $this->profileEmail = $newProfileEmail;
-}
-
-/**
- * accessor method for profileHash
- * @return string value of profile hash
- **/
-
-public function getProfileHash() {
-    return($this->profileHash);
-}
-
-/**
- * mutator method for profile hash
- *
- * @param $newProfileHash
- * @throws \InvalidArgumentException if the hash is not secure
- * @throws \RangeException if the hash is not 128 characters
- * @throws \TypeError if profile hash is not a string
- **/
-public function setProfileHash(string $newProfileHash) : void {
-    $newProfileHash = trim($newProfileHash);
-    $newProfileHash = strtolower($newProfileHash);
-    if(empty($newProfileHash) === true) {
-        throw(new \InvalidArgumentException("profile hash empty or insecure"));
-    }
-    if(!ctype_xdigit($newProfileHash)) {
-        throw(new \RangeException("profile hash must be 128 characters"));
-    }
-    $this->profileHash = $newProfileHash;
-}
-
-/**
- * accessor method for profile name
- *
- * @return string value of profile name
- **/
-public function getProfileName() : string {
-    return ($this->profileName);
-}
-
-/**
- * mutator method for profile name
- *
- * @param string $newProfileName new value of profile name
- * @throws \InvalidArgumentException if $newProfileName is not a string or is insecure
- * @throws \TypeError if $newProfileName is not a string
- * @throws \RangeException if $newProfileName is > 32 characters
- **/
-public function setProfileName(string $newProfileName) : void {
-    $newProfileName = trim($newProfileName);
-    $newProfileName = filter_var($newProfileName, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-    if(empty($newProfileName) === true) {
-        throw(new \InvalidArgumentException("profile name is empty or insecure"));
+    public function getProfileId(): Uuid
+    {
+        return $this->profileId;
     }
 
-    if(strlen($newProfileName) > 32) {
-        throw(new \RangeException("profile name is too large"));
+    /**
+     * mutator method for profile ID
+     *
+     * @param Uuid| string $newProfileId new value of profile ID
+     * @throws \RangeException if $newProfileId is not positive
+     * @throws \TypeError if $newProfileId is not a uuid or string
+     **/
+    public function setProfileId($newProfileId): void
+    {
+        try {
+            $uuid = self::validateUuid($newProfileId);
+        } catch (\InvalidArgumentException | \ RangeException | \Exception | \TypeError $exception) {
+            $exceptionType = get_class($exception);
+            throw(new $exceptionType($exception->getMessage(), 0, $exception));
+        }
+        /**
+         * convert and store the profile ID
+         **/
+        $this->profileId = $uuid;
     }
-    $this->profileName = $newProfileName;
-}
 
-/**
- *accessor method for profile salt
- *
- * @return string value of the salt
- **/
-public function getProfileSalt(): string {
-    return $this->profileSalt;
-}
-/**
- * mutator method for profile salt
- *
- * @param string $newProfileSalt
- * @throws \InvalidArgumentException if the salt is not secure
- * @throws \RangeException if the salt is not 64 characters
- * @throws \TypeError if the profile salt is not a string
- **/
-public function setProfileSalt(string $newProfileSalt): void {
-    $newProfileSalt = trim($newProfileSalt);
-    $newProfileSalt = strtolower($newProfileSalt);
-    if(!ctype_xdigit($newProfileSalt)) {
-        throw(new \InvalidArgumentException("profile password salt is empty or insecure"));
+    /**
+     *accessor method for profile email
+     * @return string value of profile email
+     **/
+
+    public function getProfileEmail(): string
+    {
+        return ($this->profileEmail);
     }
-    if(strlen($newProfileSalt) !== 64) {
-        throw(new \RangeException("profile salt must be 128 characters"));
+
+    /**
+     * mutator method for profile email
+     *
+     * @param string $newProfileEmail new value of profile email
+     * @throws \RangeException if $newProfileEmail is > 120 characters
+     * @throws \TypeError if $newProfileEmail is not a string
+     * @throws \InvalidArgumentException if $newProfileEmail is invalid or insecure
+     **/
+
+    public function setProfileEmail($newProfileEmail): void
+    {
+        $newProfileEmail = trim($newProfileEmail);
+        $newProfileEmail = filter_var($newProfileEmail, FILTER_VALIDATE_EMAIL);
+        if (empty($newProfileEmail) === true) {
+            throw(new \InvalidArgumentException("profile email is empty or insecure"));
+        }
+        if (strlen($newProfileEmail) > 128) {
+            throw(new \RangeException("profile email is too large"));
+        }
+        $this->profileEmail = $newProfileEmail;
     }
-    $this->profileSalt = $newProfileSalt;
-}
 
-/**
- * accessor method for profile statement
- *
- * @return string value of profile statement
- **/
+    /**
+     * accessor method for profileHash
+     * @return string value of profile hash
+     **/
 
-public function getProfileStatement() {
-    return($this->profileStatement);
-}
-
-/**
- * mutator method for profile statement
- *
- * @param string $newProfileStatement new value of profile statement
- * @throws \TypeError if $newProfileStatement is not a string
- * @throws \RangeException if $newProfileStatement is > 1000 characters
- **/
-
-public function setProfileStatemet(string $newProfileStatement) : void {
-    $newProfileStatement = filter_var($newProfileStatement, FILTER_SANITIZE_STRING);
-    if(strlen($newProfileStatement) > 1000) {
-        throw(new \RangeException("profile statement is too large"));
+    public function getProfileHash()
+    {
+        return ($this->profileHash);
     }
-    $this->profileStatment = $newProfileStatement;
+
+    /**
+     * mutator method for profile hash
+     *
+     * @param $newProfileHash
+     * @throws \InvalidArgumentException if the hash is not secure
+     * @throws \RangeException if the hash is not 128 characters
+     * @throws \TypeError if profile hash is not a string
+     **/
+    public function setProfileHash(string $newProfileHash): void
+    {
+        $newProfileHash = trim($newProfileHash);
+        $newProfileHash = strtolower($newProfileHash);
+        if (empty($newProfileHash) === true) {
+            throw(new \InvalidArgumentException("profile hash empty or insecure"));
+        }
+        if (!ctype_xdigit($newProfileHash)) {
+            throw(new \RangeException("profile hash must be 128 characters"));
+        }
+        $this->profileHash = $newProfileHash;
+    }
+
+    /**
+     * accessor method for profile name
+     *
+     * @return string value of profile name
+     **/
+    public function getProfileName(): string
+    {
+        return ($this->profileName);
+    }
+
+    /**
+     * mutator method for profile name
+     *
+     * @param string $newProfileName new value of profile name
+     * @throws \InvalidArgumentException if $newProfileName is not a string or is insecure
+     * @throws \TypeError if $newProfileName is not a string
+     * @throws \RangeException if $newProfileName is > 32 characters
+     **/
+    public function setProfileName(string $newProfileName): void
+    {
+        $newProfileName = trim($newProfileName);
+        $newProfileName = filter_var($newProfileName, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+        if (empty($newProfileName) === true) {
+            throw(new \InvalidArgumentException("profile name is empty or insecure"));
+        }
+
+        if (strlen($newProfileName) > 32) {
+            throw(new \RangeException("profile name is too large"));
+        }
+        $this->profileName = $newProfileName;
+    }
+
+    /**
+     *accessor method for profile salt
+     *
+     * @return string value of the salt
+     **/
+    public function getProfileSalt(): string
+    {
+        return $this->profileSalt;
+    }
+
+    /**
+     * mutator method for profile salt
+     *
+     * @param string $newProfileSalt
+     * @throws \InvalidArgumentException if the salt is not secure
+     * @throws \RangeException if the salt is not 64 characters
+     * @throws \TypeError if the profile salt is not a string
+     **/
+    public function setProfileSalt(string $newProfileSalt): void
+    {
+        $newProfileSalt = trim($newProfileSalt);
+        $newProfileSalt = strtolower($newProfileSalt);
+        if (!ctype_xdigit($newProfileSalt)) {
+            throw(new \InvalidArgumentException("profile password salt is empty or insecure"));
+        }
+        if (strlen($newProfileSalt) !== 64) {
+            throw(new \RangeException("profile salt must be 128 characters"));
+        }
+        $this->profileSalt = $newProfileSalt;
+    }
+
+    /**
+     * accessor method for profile statement
+     *
+     * @return string value of profile statement
+     **/
+
+    public function getProfileStatement()
+    {
+        return ($this->profileStatement);
+    }
+
+    /**
+     * mutator method for profile statement
+     *
+     * @param string $newProfileStatement new value of profile statement
+     * @throws \TypeError if $newProfileStatement is not a string
+     * @throws \RangeException if $newProfileStatement is > 1000 characters
+     **/
+
+    public function setProfileStatemet(string $newProfileStatement): void
+    {
+        $newProfileStatement = filter_var($newProfileStatement, FILTER_SANITIZE_STRING);
+        if (strlen($newProfileStatement) > 1000) {
+            throw(new \RangeException("profile statement is too large"));
+        }
+        $this->profileStatment = $newProfileStatement;
+    }
 }
