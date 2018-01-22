5a0fd2c6c30c057532de63add84d5f8fd887a2c7
Mary <schmitt.mary7@gmail.com>
2018-01-21 17:37:41 -0700

    updated profile class with variables

--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -6,3 +6,57 @@
  * Time: 11:45 AM
  **/
 
+namespace Edu\Cnm\DataDesign;
+
+require_once ("autoload.php");
+require_once (dirname(__DIR__, 2) . "/vendor/autoload.php");
+
+use Ramsey\Uuid\Uuid;
+
+/**
+ * Small Profile class for a site similar to Medium.
+ *
+ * This profile is a short example of a general profile class for many sites - specifically I am emulating Medium.
+ *
+ * @author Mary MacMillan <mschmitt5@cnm.edu>
+ * @author Dylan McDonald <dmcdonald21@cnm.edu>
+ *
+ **/
+
+class Profile {
+    use ValidateUuid;
+    /**
+     * ID for this profile. This will be the primary key.
+     * @var Uuid $profileId
+     */
+    private $profileId;
+    /**
+     * email for the profile
+     * @var string $profileEmail
+     */
+    private $profileEmail;
+    /**
+     * hash for validating password
+     * @var $profileHash
+     */
+    private $profileHash;
+    /**
+     * Name attached to this profile.
+     * @var string $profileName
+     */
+    private $profileName;
+    /**
+     * salt for validating password
+     * @var $profileSalt
+     */
+    private $profileSalt;
+    /**
+     * "about me" section of the profile.
+     * @var string $profileStatement
+     */
+    private $profileStatement;
+}
+
+/**
+ * accessor method for profile ID
+ */
