2d2b7c0ff855ca620de3eb9e7e0d07c16e1b5fe3
Mary <schmitt.mary7@gmail.com>
2018-01-23 06:17:24 -0700

    began work on article class

--- php/classes/Article.php
+++ php/classes/Article.php
@@ -5,3 +5,14 @@
  * Date: 1/21/2018
  * Time: 11:48 AM
  **/
+
+/**
+ * Small article class for a site similar to Medium.
+ *
+ * This profile is a short example of an article class for many blogging sites - specifically I am emulating Medium.
+ *
+ * @author Mary MacMillan <mschmitt5@cnm.edu>
+ * @author Dylan McDonald <dmcdonald21@cnm.edu>
+ *
+ **/
+
